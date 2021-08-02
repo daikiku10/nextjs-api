e8f1e9b26ab6df04da9ca623081373ed3a28c51e
daikiku10 <soccerdk19.910@gmail.com>
2021-08-02 10:50:51 +0900

    Preparing the Frontend Form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import { useRef } from "react";
+
 const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">あなたのメールアドレス</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">あなたのfeedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} ></textarea>
+        </div>
+        <button>送信 feedback</button>
+      </form>
     </div>
   );
 }
